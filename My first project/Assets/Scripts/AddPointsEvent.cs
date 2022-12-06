21baf5eca0d4cbd880445935e1cbfd017f104f3f
Gerald Landl <glandl@eurogym.info>
2022-12-06 10:16:16 +0100

    Später löschen

---
+++ My first project/Assets/Scripts/AddPointsEvent.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+using TMPro;
+
+public class AddPointsEvent : MonoBehaviour
+{
+    private UnityAction actionAddPoints;
+    private TextMeshProUGUI txtObject;
+
+    private void Awake()
+    {
+        actionAddPoints = new UnityAction(OnAddPoints);
+        txtObject = GetComponent<TextMeshProUGUI>();
+        txtObject.text = "0";
+    }
+
+    private void OnEnable()
+    {
+        EventManager.StartListening("AddPoints", actionAddPoints);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening("AddPoints", actionAddPoints);
+    }
+
+    private void OnAddPoints()
+    {
+        txtObject.text = Convert.ToString(Int32.Parse(txtObject.text) + 5);
+    }
+
+}
